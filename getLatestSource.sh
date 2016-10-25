7eae44a5a0d2b7e9ba8d4f03f46b32b11c6b80b7
nulldev <contactnulldev@gmail.com>
2016-10-25 10:35:10 -0400

    Update source fetcher. Cleanup indenting in source fetcher.

--- getLatestSource.sh
+++ getLatestSource.sh
@@ -64,93 +64,119 @@ enterPackage "eu.kanade.tachiyomi"
 
 # Get models
 enterPackage "data.database.models"
-download "Chapter.kt"
-download "Manga.kt"
-download "ChapterImpl.kt"
-download "MangaImpl.kt"
-download "Category.kt"
-download "CategoryImpl.kt"
-download "MangaSync.kt"
-download "MangaSyncImpl.kt"
-download "MangaCategory.kt"
-download "MangaChapter.kt"
+    download "Chapter.kt"
+    download "Manga.kt"
+    download "ChapterImpl.kt"
+    download "MangaImpl.kt"
+    download "Category.kt"
+    download "CategoryImpl.kt"
+    download "MangaSync.kt"
+    download "MangaSyncImpl.kt"
+    download "MangaCategory.kt"
+    download "MangaChapter.kt"
+    download "MangaChapterHistory.kt"
+leavePackage
+
+#TODO mangasync
+
+enterPackage "data.mangasync"
+    download "MangaSyncManager.kt"
+    download "MangaSyncService.kt"
+
+    enterPackage "anilist"
+        download "Anilist.kt"
+        download "AnilistApi.kt"
+        download "AnilistInterceptor.kt"
+        enterPackage "model"
+            download "ALManga.kt"
+            download "ALUserLists.kt"
+            download "ALUserManga.kt"
+            download "OAuth.kt"
+        leavePackage
+    leavePackage
+
+    enterPackage "myanimelist"
+        download "MyAnimeList.kt"
+    leavePackage
+
 leavePackage
 
 # Get source stuff
 enterPackage "data.source"
-download "Source.kt"
-download "Language.kt"
-download "SourceManager.kt"
+    download "Source.kt"
+    download "Language.kt"
+    download "SourceManager.kt"
 leavePackage
 
 enterPackage "data.source.model"
-download "Page.kt"
-download "MangasPage.kt"
+    download "Page.kt"
+    download "MangasPage.kt"
 leavePackage
 
 enterPackage "data.source.online"
-# Custom impl
-#download "OnlineSource.kt"
-download "LoginSource.kt"
-download "ParsedOnlineSource.kt"
-download "YamlOnlineSource.kt"
-download "YamlOnlineSourceMappings.kt"
-# Download all online sources but if inorichi decides to add sources in the online package directly, we are screwed, we will deal with that when it actually happens (probably won't, inorichi is smart)
-svn export https://github.com/inorichi/tachiyomi.git/trunk/app/src/main/java/eu/kanade/tachiyomi/data/source/online
-pushd "online"
-rm * # Delete all files
-cp -r * ../ # Copy out all folders
-popd
-rm -rf "online" # Delete online package
+    # Custom impl
+    #download "OnlineSource.kt"
+    download "LoginSource.kt"
+    download "ParsedOnlineSource.kt"
+    download "YamlOnlineSource.kt"
+    download "YamlOnlineSourceMappings.kt"
+
+    # Download all online sources but if inorichi decides to add sources in the online package directly, we are screwed, we will deal with that when it actually happens (probably won't, inorichi is smart)
+    svn export https://github.com/inorichi/tachiyomi.git/trunk/app/src/main/java/eu/kanade/tachiyomi/data/source/online
+    pushd "online"
+        rm * # Delete all files
+        cp -r * ../ # Copy out all folders
+    popd
+    rm -rf "online" # Delete online package
 leavePackage
 
 # Get some network utils
 enterPackage "data.network"
-download "OkHttpExtensions.kt"
-download "Requests.kt"
-download "ProgressListener.kt"
-download "NetworkHelper.kt"
-download "PersistentCookieStore.kt"
-download "PersistentCookieJar.kt"
-download "ProgressResponseBody.kt"
+    download "OkHttpExtensions.kt"
+    download "Requests.kt"
+    download "ProgressListener.kt"
+    download "NetworkHelper.kt"
+    download "PersistentCookieStore.kt"
+    download "PersistentCookieJar.kt"
+    download "ProgressResponseBody.kt"
 leavePackage
 
 # Get backup stuff (NOT USED)
 enterPackage "data.backup"
-# Custom impl
-#download "BackupManager.kt"
+    # Custom impl
+    #download "BackupManager.kt"
 leavePackage
 
 enterPackage "data.backup.serializer"
-download "IntegerSerializer.kt"
-download "IdExclusion.kt"
+    download "IntegerSerializer.kt"
+    download "IdExclusion.kt"
 leavePackage
 
 # Get some UI stuff
 enterPackage "ui.reader"
-download "ReaderChapter.kt"
+    download "ReaderChapter.kt"
 leavePackage
 
 # Get cache stuff
 enterPackage "data.cache"
-download "ChapterCache.kt"
-download "CoverCache.kt"
+    download "ChapterCache.kt"
+    download "CoverCache.kt"
 leavePackage
 
 # Get some utils
 enterPackage "util"
-download "JsoupExtensions.kt"
-download "UrlUtil.java"
-download "DiskUtils.java"
-download "ChapterRecognition.kt"
-download "DynamicConcurrentMergeOperator.java"
-download "RetryWithDelay.kt"
+    download "JsoupExtensions.kt"
+    download "UrlUtil.java"
+    download "DiskUtils.java"
+    download "ChapterRecognition.kt"
+    download "DynamicConcurrentMergeOperator.java"
+    download "RetryWithDelay.kt"
 leavePackage
 
 # Download stuff
 enterPackage "data.download.model"
-download "Download.kt"
-download "DownloadQueue.kt"
+    download "Download.kt"
+    download "DownloadQueue.kt"
 leavePackage
 
 echo -e "${UNDERLINE}${BOLD}${GRAY_BCK}${GREEN}Done!${NORMAL}"
