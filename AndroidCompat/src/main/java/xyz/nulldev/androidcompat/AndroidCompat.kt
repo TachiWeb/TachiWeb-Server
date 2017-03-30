69be55010489ea4a8307f148550edb087ba07647
nulldev <contactnulldev@gmail.com>
2017-03-29 22:35:48 -0400

    Fix Context being initialized twice.

--- AndroidCompat/src/main/java/xyz/nulldev/androidcompat/AndroidCompat.kt
+++ AndroidCompat/src/main/java/xyz/nulldev/androidcompat/AndroidCompat.kt
@@ -1,13 +1,15 @@
 package xyz.nulldev.androidcompat
 
 import android.app.Application
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.conf.global
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.lazy
 import xyz.nulldev.androidcompat.androidimpl.CustomContext
 
 class AndroidCompat {
 
-    val context by lazy {
-        CustomContext()
-    }
+    val context: CustomContext by Kodein.global.lazy.instance()
 
     fun startApp(application: Application) {
         application.attach(context)
