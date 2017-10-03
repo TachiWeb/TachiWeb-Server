cc358e7cd7afbc16e6753ca3f064dbd5f1a009db
nulldev <contactnulldev@gmail.com>
2017-10-02 22:37:32 -0400

    Replaced original Java SimpleDateFormat with IBM ICU implementation
    This fixes sources which rely on the behavior of the Android implementation of SimpleDateFormat which is identical to the IBM ICU implementation.
    
    The Java behavior of this class actually slightly differs from the Android version so nitpicky sources such as MangaSee will crash without this fix.
    
    Replacing core Java libraries requires the JVM be booted with TachiServer in the bootstrap classpath however. Thus, TachiServer will now automatically boot a new JVM with itself in the bootstrap classpath if it detects the current JVM does not have itself in the bootstrap classpath.

--- TachiServer/src/main/java/xyz/nulldev/ts/TachiServer.kt
+++ TachiServer/src/main/java/xyz/nulldev/ts/TachiServer.kt
@@ -7,6 +7,8 @@ import xyz.nulldev.androidcompat.AndroidCompat
 import xyz.nulldev.androidcompat.AndroidCompatInitializer
 import xyz.nulldev.ts.api.http.HttpAPI
 import xyz.nulldev.ts.api.http.HttpModule
+import java.lang.management.ManagementFactory
+import java.io.File
 
 /**
  * Server bootstrap class
@@ -16,7 +18,7 @@ class TachiServer {
 
     val androidCompat by lazy { AndroidCompat() }
 
-    fun run(args: Array<String>) {
+    fun initInternals() {
         //Load Android compatibility dependencies
         AndroidCompatInitializer().init()
         //Load TachiServer and Tachiyomi dependencies
@@ -26,6 +28,11 @@ class TachiServer {
 
         //Start app
         androidCompat.startApp(App())
+    }
+
+    fun run(args: Array<String>) {
+        //Initialize internals
+        initInternals()
 
         //Start UI server
         TachiWebUIServer().start()
@@ -35,9 +42,71 @@ class TachiServer {
     }
 
     companion object {
+        private const val JVM_PROPERTY = "ts.jvm"
+
         @JvmStatic
         fun main(args: Array<String>) {
+            //Check if JVM booted with bootstrap classpath
+            if(!System.getProperty(JVM_PROPERTY).equals("true", true)) {
+                try {
+                    println("JVM not booted with bootstrap classpath! Attempting to boot new JVM...")
+                    println("\tAssembling command line...")
+                    val jvmArgs = assembleBootCommand()
+                    println("\tCommand line: ${jvmArgs.joinToString(separator = " ")}")
+                    val exitCode = bootNewJvm(jvmArgs)
+                    println("\tJVM finished with exit code $exitCode")
+                    return
+                } catch(e: Exception) {
+                    println("\tAn error occurred while booting the new JVM, running TachiServer in the current JVM instead!")
+                    e.printStackTrace()
+                }
+            }
+
             TachiServer().run(args)
         }
+
+        fun assembleBootCommand(): List<String> {
+            val classpath = System.getProperty("java.class.path")
+            val classpathEntries = classpath.split(File.pathSeparator)
+
+            val bean = ManagementFactory.getRuntimeMXBean()
+            // Add boot classpath to JVM arguments
+            val jvmArgs = (bean.inputArguments + classpathEntries.map {
+                "-Xbootclasspath/p:$it"
+            }).toMutableList()
+
+            //Check if JVM booted in debug mode
+            if(bean.inputArguments.any {
+                it.startsWith("-agentlib:", true)
+            }) throw IllegalStateException("JVM booted in debug mode!")
+
+            var programArgs = System.getProperty("sun.java.command")
+
+            //Check if started from JAR
+            if(TachiServer::class.java.getResource("${TachiServer::class.simpleName}.class")
+                    .protocol == "jar") {
+                programArgs = "-jar $programArgs"
+            }
+
+            //Find Java binary
+            val javaBinFolder = File(File(System.getProperty("java.home")), "bin")
+            val windowsExec = File(javaBinFolder, "java.exe")
+            val unixExec = File(javaBinFolder, "java")
+
+            jvmArgs.add(0, when {
+                windowsExec.exists() -> windowsExec.absolutePath
+                unixExec.exists() -> unixExec.absolutePath
+                else -> throw RuntimeException("Cannot find JVM binary!")
+            })
+
+            return jvmArgs + "-D$JVM_PROPERTY=true" + programArgs.split(" ")
+        }
+
+        fun bootNewJvm(args: List<String>)
+                = ProcessBuilder()
+                .command(args)
+                .inheritIO()
+                .start()
+                .waitFor()
     }
 }
