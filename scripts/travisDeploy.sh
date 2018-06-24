4076d25bc40782eb6629a09b3f81beefe837d989
nulldev <me@nulldev.xyz>
2018-06-24 15:02:39 -0400

    Add deploy and fix mac build

---
+++ scripts/travisDeploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+curl --ftp-create-dirs \
+    -T "$(ls TachiServer/build/libs | grep TachiServer-all)" \
+    "sftp://${SFTP_USER}:${SFTP_PASSWORD}@${SFTP_HOST}${SFTP_DIR}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}_${TRAVIS_COMMIT}/server.jar"
+
+ls bootui/tachiweb-bootstrap/dist -1 | grep tachiweb- | while read x; do
+    BIN_PATH="$(realpath "bootui/tachiweb-bootstrap/dist/$x")"
+
+    curl --ftp-create-dirs \
+        -T "$BIN_PATH" \
+        "sftp://${SFTP_USER}:${SFTP_PASSWORD}@${SFTP_HOST}${SFTP_DIR}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}_${TRAVIS_COMMIT}/natives/$x"
+done
