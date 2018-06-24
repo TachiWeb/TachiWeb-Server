de5b7c787e295085948861126518134ad7a41843
nulldev <me@nulldev.xyz>
2018-06-24 12:27:55 -0400

    Integrate bootstrap submodule

--- scripts/updateSubmodules.sh
+++ scripts/updateSubmodules.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 git submodule foreach git reset --hard
-git submodule update --init --remote
-git submodule foreach "git checkout master; git pull"
+git submodule foreach "git pull"
