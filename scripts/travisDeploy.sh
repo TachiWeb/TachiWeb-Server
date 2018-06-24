c50b21b323d1d33b428aa5f3f9d809f0f492d354
nulldev <me@nulldev.xyz>
2018-06-24 19:01:34 -0400

    Attempt to fix deploy

--- scripts/travisDeploy.sh
+++ scripts/travisDeploy.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-mkdir ~/.ssh
+pushd "$TRAVIS_BUILD_DIR"
+
+mkdir -p ~/.ssh
 echo "$SFTP_KEY" | base64 --decode > ~/.ssh/id_rsa
 
-BASE_DIR="${SFTP_USER}@${SFTP_HOST}${SFTP_DIR}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}_${TRAVIS_COMMIT}"
+BASE_DIR="${SFTP_DIR}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}_${TRAVIS_COMMIT}"
+NATIVES_DIR="$BASE_DIR/natives"
+BASE_LOC="${SFTP_USER}@${SFTP_HOST}"
+
+ssh BASE_LOC "mkdir -p '$NATIVES_DIR'"
 
 mv "$(ls TachiServer/build/libs | grep TachiServer-all)" /tmp/server.jar
 
-rsync -v -e ssh /tmp/server.jar "$BASE_DIR"
+rsync -v -e ssh /tmp/server.jar "$BASE_LOC$BASE_DIR"
 
 ls -1 bootui/tachiweb-bootstrap/dist | grep -i tachiweb* | while read x; do
     BIN_PATH="$(realpath "bootui/tachiweb-bootstrap/dist/$x")"
 
-    rsync -v -e ssh "$BIN_PATH" "$BASE_DIR/natives"
+    rsync -v -e ssh "$BIN_PATH" "$BASE_LOC$NATIVES_DIR"
 done
+
+popd
