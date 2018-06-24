696d6e7a02becdac0e76d91cb3b992c9cfda93ff
nulldev <me@nulldev.xyz>
2018-06-24 16:21:07 -0400

    Various build improvements

--- scripts/travisDeploy.sh
+++ scripts/travisDeploy.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+
+# Mac OS curl hack
+export PATH=/usr/local/bin:$PATH
+
+pushd "$TRAVIS_BUILD_DIR"
+
 curl --ftp-create-dirs \
     -T "$(ls TachiServer/build/libs | grep TachiServer-all)" \
     "sftp://${SFTP_USER}:${SFTP_PASSWORD}@${SFTP_HOST}${SFTP_DIR}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}_${TRAVIS_COMMIT}/server.jar"
 
-ls bootui/tachiweb-bootstrap/dist -1 | grep tachiweb- | while read x; do
+ls bootui/tachiweb-bootstrap/dist -1 | grep -i tachiweb* | while read x; do
     BIN_PATH="$(realpath "bootui/tachiweb-bootstrap/dist/$x")"
 
     curl --ftp-create-dirs \
         -T "$BIN_PATH" \
         "sftp://${SFTP_USER}:${SFTP_PASSWORD}@${SFTP_HOST}${SFTP_DIR}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}_${TRAVIS_COMMIT}/natives/$x"
 done
+
+popd
