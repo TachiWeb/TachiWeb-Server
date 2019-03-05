b916fd742617a2fea0194272c955de96d8dce1f3
nulldev <me@nulldev.xyz>
2019-03-05 00:52:05 -0500

    Hide linux binaries and linux node modules during windows build

--- scripts/buildWindowsInDocker.sh
+++ scripts/buildWindowsInDocker.sh
@@ -1,9 +1,15 @@
+echo "Hiding linux binaries..."
+mv dist dist-linux
+echo "Removing old node modules..."
+rm -rf node_modules
+echo "Starting docker build..."
 docker run --rm -ti \
  --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
  --env ELECTRON_CACHE="/root/.cache/electron" \
  --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
  -v ${PWD}:/project \
- -v ${PWD##*/}-node-modules:/project/node_modules \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
  electronuserland/builder:wine /bin/bash -c "yarn install && yarn distWindows"
+echo "Revealing linux binaries..."
+mv dist-linux/* dist/
