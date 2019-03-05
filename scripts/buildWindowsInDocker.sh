cbcf0547527299945d229ac0573de5956566acfc
nulldev <me@nulldev.xyz>
2019-03-05 01:18:59 -0500

    Attempt to workaround wine bug

--- scripts/buildWindowsInDocker.sh
+++ scripts/buildWindowsInDocker.sh
@@ -1,7 +1,3 @@
-echo "Hiding linux binaries..."
-mv dist dist-linux
-echo "Removing old node modules..."
-rm -rf node_modules
 echo "Starting docker build..."
 docker run --rm -ti \
  --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
@@ -10,6 +6,4 @@ docker run --rm -ti \
  -v ${PWD}:/project \
  -v ~/.cache/electron:/root/.cache/electron \
  -v ~/.cache/electron-builder:/root/.cache/electron-builder \
- electronuserland/builder:wine /bin/bash -c "yarn install && yarn distWindows"
-echo "Revealing linux binaries..."
-mv dist-linux/* dist/
+ electronuserland/builder:wine /bin/bash -c "mkdir -p $HOME/.wine && yarn install && yarn distWindows"
