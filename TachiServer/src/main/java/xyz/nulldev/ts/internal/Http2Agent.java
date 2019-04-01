7f03e448a2f347adf288f45490d16dd66ac51726
nulldev <me@nulldev.xyz>
2019-04-01 15:15:43 -0400

    Ignore errors when installing ALPN agent

--- TachiServer/src/main/java/xyz/nulldev/ts/internal/Http2Agent.java
+++ TachiServer/src/main/java/xyz/nulldev/ts/internal/Http2Agent.java
@@ -2,6 +2,8 @@ package xyz.nulldev.ts.internal;
 
 import com.ea.agentloader.AgentLoader;
 import org.mortbay.jetty.alpn.agent.Premain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.instrument.Instrumentation;
 
@@ -11,14 +13,25 @@ import java.lang.instrument.Instrumentation;
  * The ALPN agent is used to inject ALPN capability into Java versions <= 1.8
  */
 public final class Http2Agent {
+    private static Logger logger = LoggerFactory.getLogger(Http2Agent.class);
+
     private Http2Agent() {
     }
 
     public static void install() {
-        AgentLoader.loadAgentClass(Http2Agent.class.getName(), "");
+        logger.info("Installing ALPN agent...");
+        try {
+            AgentLoader.loadAgentClass(Http2Agent.class.getName(), "");
+        } catch (Throwable t) {
+            logger.warn("Failed to dynamically install ALPN agent!", t);
+        }
     }
 
     public static void agentmain(String args, Instrumentation inst) throws Exception {
-        Premain.premain(args, inst);
+        try {
+            Premain.premain(args, inst);
+        } catch (Throwable t) {
+            logger.warn("ALPN agent failed to boot!", t);
+        }
     }
 }
